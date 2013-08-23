22e2d992a8416921ed5a496a3a3553e48d237db3
conord33 <conord33@gmail.com>
2013-08-22 20:01:17 -0400

    prelim gpio code

--- app.js
+++ app.js
@@ -5,13 +5,17 @@
 
 
 var io = require('socket.io-client');
+var gpio = require('gpio');
+var gpio17 = gpio.export(17);
 
 var socket = io.connect('http://localhost:3030/server');
 
 socket.on('button', function (data) {
   if (data.state == 'pressed') {
     socket.emit('buzzing', {'state':true});
+    gpio17.set();
   } else if (data.state == 'released') {
     socket.emit('buzzing', {'state':false})
+    gpio17.set(0);
   } 
 });
